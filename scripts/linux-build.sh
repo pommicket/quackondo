814bf1ab1b36a115d66e371c01fdfd6aec66a8cc
pommicket <pommicket@gmail.com>
2025-09-13 21:52:26 -0400

    Fix linux-build script

--- scripts/linux-build.sh
+++ scripts/linux-build.sh
@@ -13,9 +13,11 @@ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. || exit 1
 make -j16 || exit 1
 DIST=Quackondo
 rm -r "$DIST" || exit 1
-mkdir -p "$DIST" || exit 1
+mkdir -p "$DIST/data" || exit 1
 cp Quackle "$DIST/" || exit 1
-cp -r data "$DIST/" || exit 1
-cp -Lr "$START_PWD/macondo" "$DIST/" || exit 1
+for item in alphabets lexica strategy themes; do
+	cp -r "${START_PWD}/data/$item" "$DIST/data" || exit 1
+done
+cp -Lr "${START_PWD}/macondo" "$DIST/" || exit 1
 tar --xz -cvf 'Quackondo.tar.xz' "$DIST" || exit 1
 mv Quackondo.tar.xz "$START_PWD" || exit 1
