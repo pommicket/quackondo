4aec5480b2bbe110a75e760795e4bf6b31e5cd46
pommicket <pommicket@gmail.com>
2025-09-13 21:08:16 -0400

    Organizing, add Linux build script

---
+++ scripts/linux-build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ '!' -d macondo ]; then
+	echo 'Please copy/symlink macondo to this directory.'
+	exit 1
+fi
+
+START_PWD=$(pwd)
+
+mkdir -p quacker/build-release
+cd quacker/build-release
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. || exit 1
+make -j16 || exit 1
+DIST=Quackondo
+rm -r "$DIST" || exit 1
+mkdir -p "$DIST" || exit 1
+cp Quackle "$DIST/" || exit 1
+cp -r data "$DIST/" || exit 1
+cp -Lr "$START_PWD/macondo" "$DIST/" || exit 1
+tar --xz -cvf 'Quackondo.tar.xz' "$DIST" || exit 1
+mv Quackondo.tar.xz "$START_PWD" || exit 1
